86c57b49632801be01ee5c52d27458ffd3b07ad3
nsbhosale7 <97268765+nsbhosale7@users.noreply.github.com>
2022-04-15 01:30:04 +0530

    Delete FrontEnd_React_Code/React_App/BackEnd_Java_Code directory

--- FrontEnd_React_Code/React_App/BackEnd_Java_Code/src/main/java/com/app/custome_exception/ResourseNotFountException.java
+++
@@ -1,9 +0,0 @@
-package com.app.custome_exception;
-
-@SuppressWarnings("serial")
-public class ResourseNotFountException extends RuntimeException {
-
-	public ResourseNotFountException(String mesg) {
-		super(mesg);
-	}
-}
